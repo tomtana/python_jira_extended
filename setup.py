1cd1171b213f7ebd2b29b9120b47432502deb243
Thomas Fontana <thomas.fontana@audi.de>
2021-08-18 01:18:09 +0200

    changed to dynamix setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python_jira_extended",
+    version="0.0.1",
+    author="Thomas Fontana",
+    author_email="thomas@fontana.onl",
+    description="This package extendes the standard jira-python capabilities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tomtana/python_jira_extended",
+    project_urls={
+        "Bug Tracker": "https://github.com/tomtana/python_jira_extended/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
