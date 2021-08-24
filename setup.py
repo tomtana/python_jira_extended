d16bb008863ae2fb43660932d93bdc2022e9eb52
Thomas Fontana <thomas.fontana@audi.de>
2021-08-24 10:29:05 +0200

    Release 0.0.1

--- setup.py
+++ setup.py
@@ -20,8 +20,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
+    #package_dir={"jira_extended": "jira_extended"},
+    packages=["jira_extended"],
     python_requires=">=3.6",
     install_requires=["pandas", "numpy", "jira", "python-box"]
 )
